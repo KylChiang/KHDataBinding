371e5392a604a6cc2f48610d8fe646f9932455f2
Calvin <calvin.peak@capslock.tw>
2017-02-09 10:05:14 +0800

    Add empty auto paginating demo view controllers.

---
+++ Demo/KHDataBindingDemo/KHDataBindingDemo/AutoPaginatingCollectionViewDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  AutoPaginatingCollectionViewDemoViewController.h
+//  KHDataBindingDemo
+//
+//  Created by omg admin on 09/02/2017.
+//  Copyright © 2017 omg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AutoPaginatingCollectionViewDemoViewController : UIViewController
+
+@end
