a25708e5e02ec2fce239094b60adbb5f23dd3c5d
richard <lichang.liu@gmail.com>
2017-12-14 13:33:37 +0800

    [Add] KHCore as a singleton

---
+++ KHDataBinding/KHCore.h
@@ -0,0 +1,13 @@
+//
+//  KHCore.h
+//  KHDataBindingDemo
+//
+//  Created by richard on 2017/12/14.
+//  Copyright © 2017年 omg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KHCore : NSObject
+
+@end
---
+++ KHDataBinding/KHCore.m
@@ -0,0 +1,13 @@
+//
+//  KHCore.m
+//  KHDataBindingDemo
+//
+//  Created by richard on 2017/12/14.
+//  Copyright © 2017年 omg. All rights reserved.
+//
+
+#import "KHCore.h"
+
+@implementation KHCore
+
+@end
